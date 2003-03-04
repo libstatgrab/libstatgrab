995b00cb8e0fe22e1ee0fd89350d3f6c5e18574d
Peter Saunders <pajs@i-scream.org>
2003-03-04 18:07:42 +0000

    Useful tools, currently only one function. Function takes a file pointer and will read threw it until it hits a passed string. It will return that line.

---
+++ src/libstatgrab/tools.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+char *f_read_line(FILE *f, const char *string){
+	/* Max line length. 8k should be more than enough */
+	static char line[8192];
+
+	while((fgets(line, sizeof(line), f))==NULL){
+
+		if(strncmp(string, line, strlen(string))==0){
+			return line;
+		}
+	}
+
+	return NULL;
+}
