4e1bb4fd7593764ab1eecb61a7f9f75fe6758a07
Peter Saunders <pajs@i-scream.org>
2003-08-21 22:18:57 +0000

    Added load_stats example, and documentation.

---
+++ examples/load_stats.c
@@ -0,0 +1,52 @@
+/*
+ * i-scream central monitoring system
+ * http://www.i-scream.org.uk
+ * Copyright (C) 2000-2002 i-scream
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#include <stdio.h>
+#include <statgrab.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char **argv){
+	
+	extern char *optarg;
+        extern int optind;
+        int c;
+
+	int delay = 1;
+	load_stat_t *load_stat;
+
+	while ((c = getopt(argc, argv, "d:")) != EOF){
+                switch (c){
+                        case 'd':
+                                delay = atoi(optarg);
+                                break;
+		}
+	}
+
+	while( (load_stat = get_load_stats()) != NULL){
+		printf("Load 1 : %5.2f\t Load 5 : %5.2f\t Load 15 : %5.2f\n", load_stat->min1, load_stat->min5, load_stat->min15);
+		sleep(delay);
+	}
+
+	exit(0);
+}
+
+
+
