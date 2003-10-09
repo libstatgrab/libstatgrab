d23cc9dc451ce6e878e974467508d3e9a5e3e09a
Tim Bishop <tim@bishnet.net>
2003-10-09 16:00:31 +0000

    Standard header, plus make it include the right curses.h dependent on what the configure system finds.

--- src/saidar/saidar.c
+++ src/saidar/saidar.c
@@ -1,3 +1,27 @@
+/*
+ * i-scream central monitoring system
+ * http://www.i-scream.org
+ * Copyright (C) 2000-2002 i-scream
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
 #include <stdio.h>
 #include <string.h>
 #include <sys/ioctl.h>
@@ -10,7 +34,12 @@
 #include <sys/times.h>
 #include <limits.h>
 #include <time.h>
+
+#ifdef HAVE_NCURSES_H
+#include <ncurses.h>
+#else
 #include <curses.h>
+#endif
 
 typedef struct{
 	cpu_percent_t *cpu_percents;
