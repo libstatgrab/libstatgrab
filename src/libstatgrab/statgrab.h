e44981686b7e5cd4d508890636dd376040f00516
Peter Saunders <pajs@i-scream.org>
2004-01-09 22:06:47 +0000

    As pointed out by azz.... time_t will be needed, thus the #include of sys/types.h

--- src/libstatgrab/statgrab.h
+++ src/libstatgrab/statgrab.h
@@ -18,6 +18,8 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
+#include <sys/types.h>
+
 typedef struct{
         long long user;
         long long kernel;
