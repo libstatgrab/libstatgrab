6cf61a903ba9cd2a279204dfb42766cea5625dd6
Tim Bishop <tim@bishnet.net>
2004-02-10 20:25:54 +0000

    StatgrabException should be derived from Exception.

--- extras/python/statgrab.pyx
+++ extras/python/statgrab.pyx
@@ -135,7 +135,7 @@ class Result:
     def __repr__(self):
         return str(self.attrs)
 
-class StatgrabException:
+class StatgrabException(Exception):
     def __init__(self, value):
         self.value = value
     def __str__(self):
