db8b36c34ae68c7cf4e156b49a7233d386827dc1
Tim Bishop <tim@bishnet.net>
2004-03-06 22:37:27 +0000

    Update network traffic example to make it aware of new data.

--- examples/network_traffic.c
+++ examples/network_traffic.c
@@ -99,25 +99,35 @@ int main(int argc, char **argv){
 
 		long long total_tx=0;
 		long long total_rx=0;
+		long long total_ipackets=0;
+		long long total_opackets=0;
+		long long total_ierrors=0;
+		long long total_oerrors=0;
+		long long total_collisions=0;
 
 		for(x = 0; x < num_network_stats; x++){	
 			/* Print at location 2, linenumber the interface name */
-			printf("\033[%d;2H%-25s : %-10s", line_number++, "Network Interface Name", network_stats->interface_name);
+			printf("\033[%d;2H%-30s : %-10s", line_number++, "Network Interface Name", network_stats->interface_name);
 			/* Print out at the correct location the traffic in the requsted units passed at command time */
 			switch(units){
 				case 'b':
-					printf("\033[%d;2H%-25s : %8lld b", line_number++, "Network Interface Rx", network_stats->rx);
-					printf("\033[%d;2H%-25s : %8lld b", line_number++, "Network Interface Tx", network_stats->tx);
+					printf("\033[%d;2H%-30s : %8lld b", line_number++, "Network Interface Rx", network_stats->rx);
+					printf("\033[%d;2H%-30s : %8lld b", line_number++, "Network Interface Tx", network_stats->tx);
 					break;
 				case 'k':
-					printf("\033[%d;2H%-25s : %5lld k", line_number++, "Network Interface Rx", (network_stats->rx / 1024));
-					printf("\033[%d;2H%-25s : %5lld", line_number++, "Network Interface Tx", (network_stats->tx / 1024));
+					printf("\033[%d;2H%-30s : %5lld k", line_number++, "Network Interface Rx", (network_stats->rx / 1024));
+					printf("\033[%d;2H%-30s : %5lld", line_number++, "Network Interface Tx", (network_stats->tx / 1024));
 					break;
 				case 'm':
-					printf("\033[%d;2H%-25s : %5.2f m", line_number++, "Network Interface Rx", network_stats->rx / (1024.00*1024.00));
-					printf("\033[%d;2H%-25s : %5.2f m", line_number++, "Network Interface Tx", network_stats->tx / (1024.00*1024.00));
+					printf("\033[%d;2H%-30s : %5.2f m", line_number++, "Network Interface Rx", network_stats->rx / (1024.00*1024.00));
+					printf("\033[%d;2H%-30s : %5.2f m", line_number++, "Network Interface Tx", network_stats->tx / (1024.00*1024.00));
 			}
-			printf("\033[%d;2H%-25s : %ld ", line_number++, "Network Interface systime", (long) network_stats->systime);
+			printf("\033[%d;2H%-30s : %ld ", line_number++, "Network Interface packets in", network_stats->ipackets);
+			printf("\033[%d;2H%-30s : %ld ", line_number++, "Network Interface packets out", network_stats->opackets);
+			printf("\033[%d;2H%-30s : %ld ", line_number++, "Network Interface errors in", network_stats->ierrors);
+			printf("\033[%d;2H%-30s : %ld ", line_number++, "Network Interface errors out", network_stats->oerrors);
+			printf("\033[%d;2H%-30s : %ld ", line_number++, "Network Interface collisions", network_stats->collisions);
+			printf("\033[%d;2H%-30s : %ld ", line_number++, "Network Interface systime", (long) network_stats->systime);
 
 			/* Add a blank line between interfaces */	
 			line_number++;
@@ -125,27 +135,37 @@ int main(int argc, char **argv){
 			/* Add up this interface to the total so we can display a "total" network io" */
 			total_tx+=network_stats->tx;
 			total_rx+=network_stats->rx;
+			total_ipackets+=network_stats->ipackets;
+			total_opackets+=network_stats->opackets;
+			total_ierrors+=network_stats->ierrors;
+			total_oerrors+=network_stats->oerrors;
+			total_collisions+=network_stats->collisions;
 
 			/* Move the pointer onto the next interface. Since this returns a static buffer, we dont need
 			 * to keep track of the orginal pointer to free later */
 			network_stats++;
 		}
 
-		printf("\033[%d;2H%-25s : %-10s", line_number++, "Network Interface Name", "Total Network IO");
+		printf("\033[%d;2H%-30s : %-10s", line_number++, "Network Interface Name", "Total Network IO");
 		switch(units){
 			case 'b':
-				printf("\033[%d;2H%-25s : %8lld b", line_number++, "Network Total Rx", total_rx);
-				printf("\033[%d;2H%-25s : %8lld b", line_number++, "Network Total Tx", total_tx);
+				printf("\033[%d;2H%-30s : %8lld b", line_number++, "Network Total Rx", total_rx);
+				printf("\033[%d;2H%-30s : %8lld b", line_number++, "Network Total Tx", total_tx);
 				break;
 			case 'k':
-				printf("\033[%d;2H%-25s : %5lld k", line_number++, "Network Total Rx", (total_rx / 1024));
-				printf("\033[%d;2H%-25s : %5lld k", line_number++, "Network Total Tx", (total_tx / 1024));
+				printf("\033[%d;2H%-30s : %5lld k", line_number++, "Network Total Rx", (total_rx / 1024));
+				printf("\033[%d;2H%-30s : %5lld k", line_number++, "Network Total Tx", (total_tx / 1024));
 				break;
 			case 'm':
-				printf("\033[%d;2H%-25s : %5.2f m", line_number++, "Network Total Rx", (total_rx  / (1024.00*1024.00)));
-				printf("\033[%d;2H%-25s : %5.2f m", line_number++, "Network Total Tx", (total_tx  / (1024.00*1024.00)));
+				printf("\033[%d;2H%-30s : %5.2f m", line_number++, "Network Total Rx", (total_rx  / (1024.00*1024.00)));
+				printf("\033[%d;2H%-30s : %5.2f m", line_number++, "Network Total Tx", (total_tx  / (1024.00*1024.00)));
 				break;
 		}
+		printf("\033[%d;2H%-30s : %8lld b", line_number++, "Network Total packets in", total_ipackets);
+		printf("\033[%d;2H%-30s : %8lld b", line_number++, "Network Total packets out", total_opackets);
+		printf("\033[%d;2H%-30s : %8lld b", line_number++, "Network Total errors in", total_ierrors);
+		printf("\033[%d;2H%-30s : %8lld b", line_number++, "Network Total errors out", total_oerrors);
+		printf("\033[%d;2H%-30s : %8lld b", line_number++, "Network Total collisions", total_collisions);
 
 		fflush(stdout);
 
