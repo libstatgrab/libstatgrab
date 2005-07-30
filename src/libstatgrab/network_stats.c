0075a5a83881a42d51ee2e3e331245eb41ad1404
Adam Sampson <ats@offog.org>
2005-07-30 13:23:46 +0000

    Use <asm/types.h> to define __u32 etc., and always define u32 etc. because the Debian 2.6.0-test9 headers still use them in ethtool.h.

--- src/libstatgrab/network_stats.c
+++ src/libstatgrab/network_stats.c
@@ -49,21 +49,14 @@
 #include <sys/socket.h>
 #include <net/if.h>
 #include <ctype.h>
-/* Stuff which could be defined by defining KERNEL, but 
- * that would be a bad idea, so we'll just declare it here
- */
 #include <linux/version.h>
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,0)
-typedef __uint8_t __u8;
-typedef __uint16_t __u16;
-typedef __uint32_t __u32;
-typedef __uint64_t __u64;
-#else
+#include <asm/types.h>
+/* These aren't defined by asm/types.h unless the kernel is being
+   compiled, but some versions of ethtool.h need them. */
 typedef __uint8_t u8;
 typedef __uint16_t u16;
 typedef __uint32_t u32;
 typedef __uint64_t u64;
-#endif
 #include <linux/ethtool.h>
 #include <linux/sockios.h>
 #include <unistd.h>
