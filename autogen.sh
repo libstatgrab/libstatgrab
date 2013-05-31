85380029a6c690a6827b4379b837dde27dc66a95
Tim Bishop <tim@bishnet.net>
2013-05-31 21:56:09 +0100

    Manually pull the latest config.{guess,sub}.
    This is a temporary workaround.

--- autogen.sh
+++ autogen.sh
@@ -2,4 +2,12 @@
 
 # $Id$
 
+if [ ! -f config.guess ]; then
+	wget -O config.guess "http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=HEAD"
+fi
+
+if [ ! -f config.sub ]; then
+	wget -O config.sub "http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.sub;hb=HEAD"
+fi
+
 autoreconf -i
