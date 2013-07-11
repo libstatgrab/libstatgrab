426e76b4bbddf81a92308c2e4531687fa3bfbbc2
Tim Bishop <tim@bishnet.net>
2013-07-11 17:26:38 +0100

    Move the ChangeLog generation out of the Makefile.
    Having it in the Makefile broke the distcheck target. The specific
    failure case is if you do a VPATH configure, and then use the make
    dist target, it regenerates the ChangeLog file rather than using the
    one in $(srcdir) as it does for other distributed files. The reason it
    does this is because there's a ChangeLog target in the Makefile.
    
    This doesn't feel like a perfect solution, but it only makes life a
    little harder for devs, and fixes potential breakages for users.

--- autogen.sh
+++ autogen.sh
@@ -10,4 +10,6 @@ if [ ! -f config.sub ]; then
 	wget -O config.sub "http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.sub;hb=HEAD"
 fi
 
+git log --stat --name-only --date=short  --format='%n*%ad  %an  <%ae>%n%n%w(76,4,4)%s%n%n%b%n#Hash: %H%n#Files affected:'  | sed  -e 's/^\([^ \*]\)/    \1/'  -e 's/^    \#/  /'  -e 's/     Hash/    Hash/'  -e 's/^\*//'  | tail -n +2 > ChangeLog
+
 autoreconf -i
