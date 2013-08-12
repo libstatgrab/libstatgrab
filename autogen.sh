e5702436f8c4f14bebf6d22ecea9720bf23a4bc1
Tim Bishop <tim@bishnet.net>
2013-08-12 21:07:19 +0100

    Break the ChangeLog generation up.
    This used to be split over multiple lines when it was in the Makefile,
    but I concatenated it by mistake when bringing it across to here.

--- autogen.sh
+++ autogen.sh
@@ -2,6 +2,13 @@
 
 # $Id$
 
-git log --stat --name-only --date=short  --format='%n*%ad  %an  <%ae>%n%n%w(76,4,4)%s%n%n%b%n#Hash: %H%n#Files affected:'  | sed  -e 's/^\([^ \*]\)/    \1/'  -e 's/^    \#/  /'  -e 's/     Hash/    Hash/'  -e 's/^\*//'  | tail -n +2 > ChangeLog
+git log --stat --name-only --date=short \
+	--format='%n*%ad  %an  <%ae>%n%n%w(76,4,4)%s%n%n%b%n#Hash: %H%n#Files affected:' \
+	| sed  \
+		-e 's/^\([^ \*]\)/    \1/' \
+		-e 's/^    \#/  /' \
+		-e 's/     Hash/    Hash/' \
+		-e 's/^\*//' \
+	| tail -n +2 > ChangeLog
 
 autoreconf -i
