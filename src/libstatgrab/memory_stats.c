e31940eed2aa926f690f1a5ea4993f16c35a24a6
Tim Bishop <tim@bishnet.net>
2019-01-19 11:00:35 +0000

    On FreeBSD 12+ vmmeter.v_cache_count is removed.
    See:
    
    https://reviews.freebsd.org/D8583
    https://svnweb.freebsd.org/base?view=revision&revision=309017
    
    Dummy values (always returning 0) were added if COMPAT_FREEBSD11 is
    defined. This is defined in GENERIC, so will be there for most users.
    
    This fixes #103 by testing for vmmeter.v_cache_count and setting the
    cache value to 0 if it doesn't exist. However, more investigation may
    need to be done to see if cache can be obtained in a different way.

--- src/libstatgrab/memory_stats.c
+++ src/libstatgrab/memory_stats.c
@@ -323,10 +323,14 @@ sg_get_mem_stats_int(sg_mem_stats *mem_stats_buf) {
 		RETURN_WITH_SET_ERROR_WITH_ERRNO("mem", SG_ERROR_SYSCTLBYNAME, "vm.stats.vm.v_inactive_count");
 	}
 
+# if defined(HAVE_VMMETER_V_CACHE_COUNT)
 	size = sizeof(cache_count);
 	if (sysctlbyname("vm.stats.vm.v_cache_count", &cache_count, &size, NULL, 0) < 0) {
 		RETURN_WITH_SET_ERROR_WITH_ERRNO("mem", SG_ERROR_SYSCTLBYNAME, "vm.stats.vm.v_cache_count");
 	}
+# else
+	cache_count = 0;
+# endif
 
 	/* Of couse nothing is ever that simple :) And I have inactive pages to
 	 * deal with too. So I'm going to add them to free memory :)
